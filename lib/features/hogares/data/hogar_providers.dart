97379a25c043158722f00c0477ab7ab536166bd2
Pipedsl <h.navarrete@duocuc.cl>
2026-04-14 17:41:00 -0400

    feat(fase-2): add streamById and hogarActivoStreamProvider

---
+++ lib/features/hogares/data/hogar_providers.dart
@@ -0,0 +1,11 @@
+// lib/features/hogares/data/hogar_providers.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:despensa_inteligente/features/auth/data/usuario_providers.dart';
+import 'package:despensa_inteligente/features/hogares/data/hogar_repository.dart';
+import 'package:despensa_inteligente/features/hogares/domain/hogar.dart';
+
+final hogarActivoStreamProvider = StreamProvider.autoDispose<Hogar?>((ref) {
+  final hogarId = ref.watch(usuarioStreamProvider).asData?.value?.hogarActivo;
+  if (hogarId == null) return Stream.value(null);
+  return ref.watch(hogarRepositoryProvider).streamById(hogarId);
+});
